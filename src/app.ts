dd98e08e4b9818321a430cfff53cf49c7a67d540
Arthur Madureira <arthurmadureira10@gmail.com>
2023-07-20 23:49:44 -0300

    chore: create user route

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,28 @@
 import Fastify from 'fastify'
+import { z } from 'zod'
+import { prisma } from './lib/prisma'
 
 export const app = Fastify()
 
 
-app.get('/', (request, reply) => {
-    reply.send({
-        message: 'Hello World'
+app.post('/users', async (request, reply) => {
+    const registerBodySchema = z.object({
+        name: z.string(),
+        email: z.string().email(),
+        password: z.string().min(6),
     })
-})
 
+    const { name, email, password } = registerBodySchema.parse(request.body)
+
+    await prisma.user.create({
+        data: {
+            name,
+            email,
+            password_hash: password,
+        }
+    })
+
+    return reply.status(201).send()
+
+})
 
