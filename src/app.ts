27486e4a771eb385931379c858a0cf01dc510260
Arthur Madureira <arthurmadureira10@gmail.com>
2023-07-24 00:05:08 -0300

    chore: register use case

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import Fastify from 'fastify'
 import { appRoutes } from './http/routes'
 
+// Controller = Função que lida com entrada de dados de uma requisição HTTP e retorna uma resposta
+
 export const app = Fastify()
 
 app.register(appRoutes)
