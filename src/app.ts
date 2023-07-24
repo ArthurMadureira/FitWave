33b238c84cd1bbbf8d703c5963b06e9eb1738333
Arthur Madureira <arthurmadureira10@gmail.com>
2023-07-24 00:29:54 -0300

    chore: implementating repository pattern

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import Fastify from 'fastify'
 import { appRoutes } from './http/routes'
 
 // Controller = Função que lida com entrada de dados de uma requisição HTTP e retorna uma resposta
+//Repositories = Toda modificação que faremos no banco de dados
 
 export const app = Fastify()
 
