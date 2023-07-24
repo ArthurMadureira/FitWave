d9a0db38d3c5cbc84a02288b339c6179d60cd89a
Arthur Madureira <arthurmadureira10@gmail.com>
2023-07-24 17:12:46 -0300

    chore: create global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import Fastify from 'fastify'
 import { appRoutes } from './http/routes'
+import { ZodError } from 'zod'
+import { env } from './env'
 
 // Controller = Função que lida com entrada de dados de uma requisição HTTP e retorna uma resposta
 //Repositories = Toda modificação que faremos no banco de dados
@@ -7,4 +9,21 @@ import { appRoutes } from './http/routes'
 export const app = Fastify()
 
 app.register(appRoutes)
+
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    return reply.status(400).send({
+      message: 'Validation error',
+      issues: error.format()
+    })
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.error(error)
+  } else {
+    // TODO: Here should log to an external tool like DataDog/NewRelic/Sentry
+  }
+
+  return reply.status(500).send({ message: 'Internal server error' })
+})
 
