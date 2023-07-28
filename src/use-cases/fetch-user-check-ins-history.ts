40ba5c6da2cad22526a184909a398f355b173f7d
Arthur Madureira <arthurmadureira10@gmail.com>
2023-07-28 00:20:17 -0300

    test: All data lists need to be paginated with 20 items per page

--- src/use-cases/fetch-user-check-ins-history.ts
+++ src/use-cases/fetch-user-check-ins-history.ts
@@ -3,20 +3,22 @@ import { CheckIn } from '@prisma/client'
 
 interface FetchUserCheckInsHistoryUseCaseRequest {
   userId: string
+  page: number
 }
 
 interface FetchUserCheckInsHistoryUseCaseResponse {
   checkIns: CheckIn[]
 }
 
-export class CheckUserCheckInsHistoryUseCase {
+export class FetchUserCheckInsHistoryUseCase {
   constructor(private checkInsRepository: CheckInsRepository,) { }
 
   async execute({
-    userId
+    userId,
+    page
   }: FetchUserCheckInsHistoryUseCaseRequest): Promise<FetchUserCheckInsHistoryUseCaseResponse> {
 
-    const checkIns = await this.checkInsRepository.findManyByUserId(userId)
+    const checkIns = await this.checkInsRepository.findManyByUserId(userId, page)
 
     return {
       checkIns
