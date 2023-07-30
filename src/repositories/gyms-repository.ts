a2bcd1b0b134d0c77eeb00c4c01da59114b23e1e
Arthur Madureira <arthurmadureira10@gmail.com>
2023-07-29 23:02:51 -0300

    chore: create gym repository

--- src/repositories/gyms-repository.ts
+++ src/repositories/gyms-repository.ts
@@ -10,5 +10,5 @@ export interface GymsRepository {
   findManyNearby(params: findManyNearbyParams): Promise<Gym[]>
   searchMany(query: string, page: number): Promise<Gym[]>
   create(data: Prisma.GymCreateInput): Promise<Gym>
-  items: Gym[]
+  items?: Gym[]
 }
