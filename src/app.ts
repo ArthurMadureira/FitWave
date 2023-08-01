0a98b8819b232f32d0ed194d525b56244c23081b
Arthur Madureira <arthurmadureira10@gmail.com>
2023-08-01 13:05:01 -0300

    test: gym routes e2e tests

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import { ZodError } from 'zod'
 import { env } from './env'
 import { usersRoutes } from './http/controllers/users/routes'
 import { gymsRoutes } from './http/controllers/gyms/routes'
+import { checkInsRoutes } from './http/controllers/check-ins/routes'
 
 // Controller = Função que lida com entrada de dados de uma requisição HTTP e retorna uma resposta
 //Repositories = Toda modificação que faremos no banco de dados
@@ -17,6 +18,7 @@ app.register(fastifyJwt, {
 
 app.register(usersRoutes)
 app.register(gymsRoutes)
+app.register(checkInsRoutes)
 
 app.setErrorHandler((error, _, reply) => {
   if (error instanceof ZodError) {
